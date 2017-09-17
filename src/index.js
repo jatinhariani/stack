3666dac7618beb620f8ec9ed1ba6f09fdc833d46
Jatin Hariani <jtnh321@gmail.com>
2017-09-17 22:35:00 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
