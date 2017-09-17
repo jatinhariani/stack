9bdf47b882c3fb7203fa31e6d0dfffac2280cf8b
Jatin Hariani <jtnh321@gmail.com>
2017-09-17 22:20:27 +0530

    eslint requirements and express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+import express from 'express'
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000')
+})
