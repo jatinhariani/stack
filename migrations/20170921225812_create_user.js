801e0aa1984afe2f808ebcd460ff1c5dff645b48
Jatin Hariani <jtnh321@gmail.com>
2017-09-23 22:45:25 +0530

    unique email;

--- migrations/20170921225812_create_user.js
+++ migrations/20170921225812_create_user.js
@@ -1,7 +1,7 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.increments()
-    table.string('email').notNullable()
+    table.string('email').notNullable().unique()
     table.string('password').notNullable()
     table.string('givenName')
     table.string('familyName')
